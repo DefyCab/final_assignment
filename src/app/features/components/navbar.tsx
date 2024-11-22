c19810a6e4abc51ffe21c13d1af19f60642625ce
defycab <love.bellinder@gmail.com>
2024-11-22 13:38:50 +0100

    add feature and components folder. make navbar component

---
+++ src/app/features/components/navbar.tsx
@@ -0,0 +1,3 @@
+export function Navbar() {
+  return <nav className="navbar bg-slate-200"></nav>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "./features/components/navbar";
+
 export default function Home() {
-  return <h1>Hello Voter</h1>
+  return <Navbar />;
 }
