55ec23078fcf8025522fbecbf2b0bd302ab917ee
defycab <love.bellinder@gmail.com>
2024-11-22 20:02:45 +0100

    change name on opinion button

--- src/app/elections/page.tsx
+++ src/app/elections/page.tsx
@@ -4,19 +4,19 @@ export default function Elections() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col h-[calc(100vh-64px)]">
+      <main className="flex flex-col h-[calc(100vh-118px)]">
         <h1 className="font-bold text-center text-2xl mt-1">Elections</h1>
         <div className="mt-4 ml-1 mr-1 flex flex-row justify-between">
           <p className="text-decoration-line: underline">Issue</p>
           <p className="text-decoration-line: underline">Date Created</p>
           <p className="text-decoration-line: underline">Status</p>
         </div>
-        <div className="flex justify-center gap-4 ">
+      </main>
+        <div className="flex justify-center gap-4">
           <button className="btn btn-primary">Create Election</button>
-          <button className="btn btn-primary">Show Opinion</button>
+          <button className="btn btn-primary">Register Opinion</button>
           <button className="btn btn-warning">Close Election</button>
         </div>
-      </main>
     </>
   );
 }
