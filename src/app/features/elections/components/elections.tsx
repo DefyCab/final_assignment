37b49f55b5d86b416dd9ef23c5d8ae70defb4b1f
defycab <love.bellinder@gmail.com>
2024-11-22 22:52:04 +0100

    map out issues in elections

--- src/app/features/elections/components/elections.tsx
+++ src/app/features/elections/components/elections.tsx
@@ -9,13 +9,18 @@ export async function Elections() {
     <>
       <main className="mx-auto flex flex-col h-[calc(100vh-118px)]">
         <h1 className="font-bold text-center text-2xl mt-1">Elections</h1>
-        <div className="mt-4 ml-1 mr-1 flex flex-row justify-between">
+        <div className="mt-4 flex flex-row justify-between">
           <p className="text-decoration-line: underline">Issue</p>
           <p className="text-decoration-line: underline">Date Created</p>
           <p className="text-decoration-line: underline">Status</p>
         </div>
+        <article>
+          {elections.map((election) => (
+            <p key={election.id}>{election.issue}</p>
+          ))}
+        </article>
       </main>
-      <div className="flex justify-center gap-4 m-1">
+      <div className="flex justify-center gap-4">
         <button className="btn btn-primary">Create Election</button>
         <button className="btn btn-primary">Register Opinion</button>
         <button className="btn btn-warning">Close Election</button>
