4db8b00bd9f682f1913dcf8d0d6a840ef51528b5
defycab <love.bellinder@gmail.com>
2024-11-22 19:30:18 +0100

    adding elements and basic styling to elections page

--- src/app/elections/page.tsx
+++ src/app/elections/page.tsx
@@ -1,3 +1,17 @@
+import { Navbar } from "../features/components/navbar";
+
 export default function Elections() {
-  return <p>Elections</p>
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1 className="font-bold text-center text-2xl mt-1">Elections</h1>
+        <div className="mt-4 ml-1 mr-1 flex flex-row justify-between">
+          <p className="text-decoration-line: underline">Issue</p>
+          <p className="text-decoration-line: underline">Date Created</p>
+          <p className="text-decoration-line: underline">Status</p>
+        </div>
+      </main>
+    </>
+  );
 }
