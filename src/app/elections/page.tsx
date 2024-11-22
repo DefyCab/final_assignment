44f9e2b527149655ced0e0799e1da7f6e78343b5
defycab <love.bellinder@gmail.com>
2024-11-22 19:55:07 +0100

    add theme nord

--- src/app/elections/page.tsx
+++ src/app/elections/page.tsx
@@ -4,7 +4,7 @@ export default function Elections() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col h-screen">
+      <main className="flex flex-col h-[calc(100vh-64px)]">
         <h1 className="font-bold text-center text-2xl mt-1">Elections</h1>
         <div className="mt-4 ml-1 mr-1 flex flex-row justify-between">
           <p className="text-decoration-line: underline">Issue</p>
@@ -14,7 +14,7 @@ export default function Elections() {
         <div className="flex justify-center gap-4 ">
           <button className="btn btn-primary">Create Election</button>
           <button className="btn btn-primary">Show Opinion</button>
-          <button className="btn btn-secondary">Close Election</button>
+          <button className="btn btn-warning">Close Election</button>
         </div>
       </main>
     </>
