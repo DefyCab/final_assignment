b37ca1b3154be63eeba299727d6c3232f5e25f82
defycab <love.bellinder@gmail.com>
2024-11-23 18:54:58 +0100

    adding options to fixture and to election component

--- src/app/features/elections/components/election.tsx
+++ src/app/features/elections/components/election.tsx
@@ -6,13 +6,33 @@ export async function Election() {
   );
 
   return (
-    <main className="flex justify-center">
-      <section className="w-80 h-48 bg-base-300 mt-4 border-solid border-2 border-primary rounded">
-        <h1>Election</h1>
+    <main className="mr-4 ml-4 flex justify-center">
+      <section className="w-full h-[calc(100vh-112px)] bg-base-300 mt-4 border-solid border-2 border-primary rounded">
         <article>
           {election.map((e) => (
-            <p key={e.id}>{e.issue}</p>
+            <h3 className="font-bold text-2xl text-center" key={e.id}>
+              {e.issue}
+            </h3>
           ))}
+          <div className="m-2 flex justify-between">
+            <p className="font-semibold">Options</p>
+            <p className="font-semibold">Number of Votes</p>
+          </div>
+          {election.map((e) =>
+            e.options?.map((option) => (
+              <p className="text-sm ml-2">{option.one}</p>
+            ))
+          )}
+          {election.map((e) =>
+            e.options?.map((option) => (
+              <p className="text-sm ml-2">{option.two}</p>
+            ))
+          )}
+          {election.map((e) =>
+            e.options?.map((option) => (
+              <p className="text-sm ml-2">{option.three}</p>
+            ))
+          )}
         </article>
       </section>
     </main>
