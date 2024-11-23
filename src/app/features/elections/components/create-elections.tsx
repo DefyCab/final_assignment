ddd75fe95414506e6b145c71ff2c8b4640309476
defycab <love.bellinder@gmail.com>
2024-11-23 11:22:59 +0100

    add button to createElection form

--- src/app/features/elections/components/create-elections.tsx
+++ src/app/features/elections/components/create-elections.tsx
@@ -1,9 +1,10 @@
 export function CreateElection() {
   return (
     <>
-      <form className="flex flex-col justify-center items-center">
-        <label htmlFor="issue">Issue</label>
+      <form className="flex flex-col justify-center items-center m-1">
+        <label className="m-1" htmlFor="issue">Issue</label>
         <input className="w-72" type="text" name="issue" />
+        <button className="btn btn-accent mt-4">Create Election</button>
       </form>
     </>
   );
