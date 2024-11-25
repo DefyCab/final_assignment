8af3dd02283d5dd7d6c2c8e23ac3d2b600dc680a
defycab <love.bellinder@gmail.com>
2024-11-25 09:43:16 +0100

    fix navbar broken imports

--- src/app/elections/create/page.tsx
+++ src/app/elections/create/page.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/app/features";
+import { Navbar } from "@/app/features/elections";
 import { CreateElection } from "@/app/features/elections/components/create-elections";
 
 export default function Page() {
