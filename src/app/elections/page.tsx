b671ebdea6073956944ba4fbba47c360a0b5f31e
defycab <love.bellinder@gmail.com>
2024-12-01 11:43:26 +0100

    removing unnecessary code

--- src/app/elections/page.tsx
+++ src/app/elections/page.tsx
@@ -3,12 +3,7 @@ import { Navbar } from "../../features/elections";
 import { electionService } from "@/features/elections/instance";
 
 export default async function Page() {
-  const elections: any = [];
-  const electionsmap = await electionService.getAll();
-
-  electionsmap.map((election) => {
-    elections.push(election);
-  });
+  const elections = await electionService.getAll();
 
   return (
     <>
