7d380a50a24e248c3533709c83582dbc55bac232
defycab <love.bellinder@gmail.com>
2024-12-09 01:38:52 +0100

    adding try catch to elections repository

--- src/features/elections/repository.ts
+++ src/features/elections/repository.ts
@@ -17,31 +17,45 @@ export type Election = z.infer<typeof ElectionsSchema>;
 export function createRepository(db: Db) {
   return {
     getAll: async () => {
-      return await db
-        .select()
-        .from(elections)
-        .limit(10)
-        .orderBy(asc(elections.createdAt));
+      try {
+        return await db
+          .select()
+          .from(elections)
+          .limit(10)
+          .orderBy(asc(elections.createdAt));
+      } catch (error) {
+        console.log(error);
+      }
     },
     get: async (id: string) => {
-      return await db.select().from(elections).where(eq(elections.id, id));
+      try {
+        return await db.select().from(elections).where(eq(elections.id, id));
+      } catch (error) {
+        console.log(error);
+      }
     },
     create: async (election: CreateElection) => {
       return await db.insert(elections).values(election);
     },
     update: async (id: string) => {
-      const election = await electionService.get(id);
+      try {
+        const election = await electionService.get(id);
 
-      if (election[0].status === false) {
-        return;
+        if (!election) return console.log("Election not found");
+
+        if (election[0].status === false) {
+          return;
+        }
+
+        return await db
+          .update(elections)
+          .set({
+            ...(election[0].status && { status: false }),
+          })
+          .where(eq(elections.id, id));
+      } catch (error) {
+        console.log(error);
       }
-
-      return await db
-        .update(elections)
-        .set({
-          ...(election[0].status && { status: false }),
-        })
-        .where(eq(elections.id, id));
     },
   };
 }
