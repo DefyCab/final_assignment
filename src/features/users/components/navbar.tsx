457a7edce623407534466dcf726c986bc7fa1a9e
defycab <love.bellinder@gmail.com>
2024-12-09 02:34:31 +0100

    add hardcoded user as logged in

--- src/features/users/components/navbar.tsx
+++ src/features/users/components/navbar.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 
 export function Navbar() {
+  const user = "Love Ericson";
   return (
-    <nav className="navbar bg-primary justify-end w-full">
-      <Link className="p-2" href="\representatives\">
-        Representatives
-      </Link>
-      <Link className="p-2" href="\elections\">
-        Elections
-      </Link>
-    </nav>
+    <>
+      <nav className="navbar bg-primary justify-between w-full">
+        <div className="flex justify-start">
+          <p className="text-warning font-bold">{user}</p>
+        </div>
+        <div>
+          <Link className="p-2 hover:text-slate-200" href="\representatives\">
+            Representatives
+          </Link>
+          <Link className="p-2 hover:text-slate-200" href="\elections\">
+            Elections
+          </Link>
+        </div>
+      </nav>
+    </>
   );
 }
