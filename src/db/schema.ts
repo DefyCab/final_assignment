9ad2eb57ccdddbb6c02586c6801e1c16d0628124
defycab <love.bellinder@gmail.com>
2024-12-11 14:22:59 +0100

    delete code from original schema leaving only vote schema for later consideration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,31 +1,4 @@
-import { sql } from "drizzle-orm";
-import {
-  pgTable,
-  boolean,
-  uuid,
-  text,
-  timestamp,
-  jsonb,
-  varchar,
-  integer,
-} from "drizzle-orm/pg-core";
-
-export const elections = pgTable("elections", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  issue: text("issue").notNull(),
-  options: jsonb("options").$type<string[]>().notNull(),
-  createdAt: timestamp("createdAt", { mode: "string" })
-    .notNull()
-    .default(sql`now()`),
-  status: boolean().notNull(),
-});
-
-export const users = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: varchar("name").notNull(),
-  email: varchar("email", { length: 255 }).unique().notNull(),
-  representative: boolean().notNull(),
-});
+import { pgTable, uuid, integer } from "drizzle-orm/pg-core";
 
 export const votes = pgTable("votes", {
   user_id: uuid("id").primaryKey(),
