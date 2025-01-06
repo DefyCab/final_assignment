71498a706f5d2fc37d30dfc7a9d87851a18af15e
defycab <love.bellinder@gmail.com>
2025-01-06 10:38:32 +0100

    list open elections

--- src/features/elections/components/register-opinion.tsx
+++ src/features/elections/components/register-opinion.tsx
@@ -1,13 +1,43 @@
+"use client";
 import type { Election } from "../repository";
+import { useRouter } from "next/navigation";
 
 type Props = {
   elections: Election[];
 };
 
 export function RegisterOpinion({ elections }: Props) {
+  const router = useRouter();
+
+  const openElections = elections.filter(
+    (election) => election.status === true
+  );
+
   return (
-    <div>
-      <article></article>
-    </div>
+    <>
+      <h1 className="font-bold text-center text-2xl mt-1">Open Elections</h1>
+      <div className="mt-4 flex flex-row justify-between">
+        <article className="cursor-pointer">
+          <p className="font-semibold text-decoration-line: underline">Issue</p>
+          {openElections.map((election) => (
+            <p
+              key={election.id}
+              onClick={() => router.push(`/elections/election/${election.id}`)}
+            >
+              {election.issue}
+            </p>
+          ))}
+        </article>
+
+        <article>
+          <p className="font-semibold text-decoration-line: underline">
+            Date Created
+          </p>
+          {openElections.map((election) => (
+            <p key={election.id}>{election.createdAt.slice(0, 10)}</p>
+          ))}
+        </article>
+      </div>
+    </>
   );
 }
