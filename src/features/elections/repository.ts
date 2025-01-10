56fd141f4196cb567d97af1004fd0332c67b44ea
defycab <love.bellinder@gmail.com>
2025-01-10 10:49:29 +0100

    move zod validation on getAll method

--- src/features/elections/repository.ts
+++ src/features/elections/repository.ts
@@ -1,48 +1,14 @@
 import { Db, electionService } from "./instance";
 import { elections } from "./db";
 import { desc, eq } from "drizzle-orm";
-import { z } from "zod";
-
-const electionsSchema = z.object({
-  id: z.string().uuid(),
-  issue: z.string(),
-  options: z.array(z.string()),
-  createdAt: z.string(),
-  status: z.boolean(),
-});
-
-const createElectionsSchema = z.object({
-  issue: z.string(),
-  options: z.array(z.string()),
-  status: z.boolean(),
-});
-
-export type Election = z.infer<typeof electionsSchema>;
-export type CreateElection = z.infer<typeof createElectionsSchema>;
 
 export function createRepository(db: Db) {
   return {
     getAll: async () => {
-      try {
-        const data = await db
-          .select()
-          .from(elections)
-          .orderBy(desc(elections.createdAt));
-
-        const electionsArray = z.array(electionsSchema);
-
-        const electionsToValidate = electionsArray.safeParse(data);
-
-        if (!electionsToValidate.success) {
-          console.log(electionsToValidate.error.message);
-        }
-        return {
-          data: electionsToValidate.data,
-          tags: ["elections"],
-        };
-      } catch (error) {
-        console.log(error);
-      }
+      return await db
+        .select()
+        .from(elections)
+        .orderBy(desc(elections.createdAt));
     },
     get: async (id: string) => {
       try {
--- src/features/elections/service.ts
+++ src/features/elections/service.ts
@@ -1,12 +1,44 @@
 import { Db } from "./instance";
-import { CreateElection, createRepository } from "./repository";
+import { createRepository } from "./repository";
 import type { User } from "./types";
+import { z } from "zod";
+
+const electionsSchema = z.object({
+  id: z.string().uuid(),
+  issue: z.string(),
+  options: z.array(z.string()),
+  createdAt: z.string(),
+  status: z.boolean(),
+});
+
+const createElectionsSchema = z.object({
+  issue: z.string(),
+  options: z.array(z.string()),
+  status: z.boolean(),
+});
+
+export type Election = z.infer<typeof electionsSchema>;
+export type CreateElection = z.infer<typeof createElectionsSchema>;
 
 export function createService(db: Db, userService: any) {
   const repository = createRepository(db);
-
   return {
     getAll: async () => {
+      const data = repository.getAll();
+      const electionsArray = z.array(electionsSchema);
+      try {
+        const electionsToValidate = electionsArray.safeParse(data);
+
+        if (!electionsToValidate.success) {
+          console.log(electionsToValidate.error.message);
+        }
+        return {
+          data: electionsToValidate.data,
+          tags: ["elections"],
+        };
+      } catch (error) {
+        console.log(error);
+      }
       return await repository.getAll();
     },
     get: async (id: string) => {
